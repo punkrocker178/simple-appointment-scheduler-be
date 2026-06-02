d9d606b948a2a08a3272c8c77a0ccd0cdfde8a7d
Hieu Le <ngochieu.le1708@gmail.com>
2026-06-02 15:01:46 +0700

    Add launch profile. Remove obj and bin

--- Infrastructure/Persistence/DesignTimeDbContextFactory.cs
+++ Infrastructure/Persistence/DesignTimeDbContextFactory.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using Infrastructure.Data;
 
 namespace Infrastructure.Data;
 
